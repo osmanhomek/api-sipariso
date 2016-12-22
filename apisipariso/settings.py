77dfce7747ea7a8619203590770c4ef1b0cead56
Osman Homek <osman@geonni.com>
2016-12-22 15:22:16 +0300

    first15

--- apisipariso/settings.py
+++ apisipariso/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'apisipariso.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, "static/")
+STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
