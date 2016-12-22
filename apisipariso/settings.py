25dbeb4331d00c92e7a8205da844168780ea82b6
Osman Homek <osman@geonni.com>
2016-12-22 15:15:22 +0300

    first14

--- apisipariso/settings.py
+++ apisipariso/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'apisipariso.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [templates],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
